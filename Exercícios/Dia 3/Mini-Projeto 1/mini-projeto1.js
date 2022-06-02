892758b1094b2a2f8e0d827de918d1e05528289e
Ramon Gabriel <ramongtstudy@gmail.com>
2022-06-02 20:41:22 -0300

    Primeira atividade concluída

---
+++ Exercícios/Dia 3/Mini-Projeto 1/mini-projeto1.js
@@ -0,0 +1,18 @@
+// Variáveis
+let a = 20;
+let b = 30;
+
+//Mini-Projeto 1: Trocando Valores de Variáveis
+console.log('Troque os valores: a = 20 / b = 30')
+
+//Solução:
+let c = a;
+a = b;
+b = c;
+
+//Resultado:
+console.log('a =',a);
+console.log('b =',b);
+
+//Espaço
+console.log(' ')
