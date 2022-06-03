7e23ebbdf881c39099525678a24e29714b2f404a
Ramon Gabriel <ramongtstudy@gmail.com>
2022-06-03 17:04:29 -0300

    Adição da segunda atividade

---
+++ Exercícios/Mini-Projeto 2/mini-projeto2.js
@@ -0,0 +1,14 @@
+// Mini-Projeto 2:
+// Escreva uma função que usa 2 números e retorna o maior
+
+//Variáveis
+let valorMaior = valores(9,10)
+console.log('O maior valor é:',valorMaior)
+
+//Função
+function valores(num1,num2) {
+    if(num1 > num2)
+        return num1;
+    else 
+        return num2;
+}
