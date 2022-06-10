7f6cf906f649fa5bbf442d293b99baab536201c7
Ramon Gabriel <ramongtstudy@gmail.com>
2022-06-10 13:25:57 -0300

    Complemento TDD

--- Mocha-Chai/src/Calculadora.js
+++ Mocha-Chai/src/Calculadora.js
@@ -4,8 +4,17 @@ export default class Calculadora {
     static soma(a, b) {
         return a + b
     }
+
     static sub(a, b) {
         return a - b
+    }
+
+    static multi(a, b) {
+        return a * b
+    }
+    
+    static div(a, b) {
+        return a / b
     }
     
 }
--- Mocha-Chai/test/calculadora.spec.js
+++ Mocha-Chai/test/calculadora.spec.js
@@ -19,4 +19,30 @@ describe('Testes de subtração', () => {
         let resultado = Calculadora.sub(4, 5)
         expect(resultado).to.be.eq(-1)
     })
+    it('Deve subtrair 50 e 10 resultando em 40', () => {
+        let resultado = Calculadora.sub(50, 10)
+        expect(resultado).to.be.eq(40)
+    })
+})
+
+describe('Testes de multiplicação', () => {
+    it('Deve multiplicar 6 e 5 resultando em 30', () => {
+        let resultado = Calculadora.multi(6, 5)
+        expect(resultado).to.be.eq(30)
+    })
+    it('Deve multiplicar 10 e 50 resultando em 500', () => {
+        let resultado = Calculadora.multi(10, 50)
+        expect(resultado).to.be.eq(500)
+    })
+})
+
+describe('Testes de divisão', () => {
+    it('Deve dividir 10 e 5 resultando em 2', () => {
+        let resultado = Calculadora.div(10, 5)
+        expect(resultado).to.be.eq(2)
+    })
+    it('Deve dividir 80 e 4 resultando em 20', () => {
+        let resultado = Calculadora.div(80, 4)
+        expect(resultado).to.be.eq(20)
+    })
 })
